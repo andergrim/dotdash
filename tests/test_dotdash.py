17c97c2d5e84b10ce66cbb0a437607d7e95b781b
Kristoffer Andergrim <kristoffer.andergrim@wirelesscar.com>
2024-02-16 11:36:52 +0100

    Test command exists

--- tests/test_dotdash.py
+++ tests/test_dotdash.py
@@ -4,7 +4,7 @@
 import pytest
 from click.testing import CliRunner
 
-from dotdash import cli
+from dotdash import cli, __version__
 
 
 @pytest.fixture
@@ -15,7 +15,6 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
@@ -30,6 +29,29 @@ def test_command_line_interface():
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
     assert 'dotdash' in result.output
+
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help     Show this message and exit.' in help_result.output
+
+    version_result = runner.invoke(cli.main, ['--version'])
+    assert version_result.exit_code == 0
+    assert f'main, version {__version__}' in version_result.output
+
+def test_commands_exists():
+    """Test existence of each command"""
+    commands = [
+        "adopt",
+        "archive",
+        "check",
+        "evict",
+        "init",
+        "mv",
+        "reset",
+        "sync",
+    ]
+
+    for cmd in commands:
+        runner = CliRunner()
+        result = runner.invoke(cli.main, [cmd, "--help"])
+        assert result.exit_code == 0
